fff8c8803b46f318c6052800968995e92fdd015a
AshkanZj <ashkanzojaji19@gmail.com>
2024-02-29 17:14:51 +0330

    add main container and fixed sidebar

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-
-const arrowUp = document.getElementById('arrowup');
-const iconLocation = document.getElementById('iconlocation');
-const inputLocation = document.getElementById('inputlocation');
-iconLocation.addEventListener('click', arrowUpIcon);
+const arrowUp = document.getElementById("arrowup");
+const iconLocation = document.getElementById("iconlocation");
+const inputLocation = document.getElementById("inputlocation");
+iconLocation.addEventListener("click", arrowUpIcon);
 
 function arrowUpIcon() {
-  arrowUp.classList.toggle('active');
-  inputLocation.classList.toggle('hide');
-
+  arrowUp.classList.toggle("active");
+  inputLocation.classList.toggle("hide");
+}
 const products = [
   {
     name: "کابینت",
