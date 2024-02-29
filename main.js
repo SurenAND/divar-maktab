dc8b10f05e711be0a1f865e80bfa0480a0e31410
moayad-zanganeh <moayadzangane@example.com>
2024-02-29 16:29:30 +0330

    starts in javaScript in functional arrowUp and arrowDown location icons

---
+++ main.js
@@ -0,0 +1,8 @@
+let arrowUp = document.getElementById('arrowup');
+let arrowDown = document.getElementById('arrowdown');
+
+arrowUp.addEventListener('click', arrowUpIcon);
+
+function arrowUpIcon() {
+  arrowUp.style.display = 'none';
+}
